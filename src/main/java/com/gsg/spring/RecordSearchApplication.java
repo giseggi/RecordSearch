3c77a42648489c6f5b14e0ffc9a5a9d70bc4c30d
gisegg <giseggi57@gmail.com>
2022-03-06 01:51:51 +0900

    first commit

---
+++ src/main/java/com/gsg/spring/RecordSearchApplication.java
@@ -0,0 +1,13 @@
+package com.gsg.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordSearchApplication.class, args);
+	}
+
+}
