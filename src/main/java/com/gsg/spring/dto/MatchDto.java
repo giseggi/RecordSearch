e0d1d4e2993650e974390244707389823c7fc93f
gisegg <giseggi57@gmail.com>
2022-03-30 00:26:19 +0900

    Add items in MatchDto

--- src/main/java/com/gsg/spring/dto/MatchDto.java
+++ src/main/java/com/gsg/spring/dto/MatchDto.java
@@ -1,5 +1,7 @@
 package com.gsg.spring.dto;
 
+import java.util.List;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,11 +11,53 @@ public class MatchDto {
 
 	// Champion Id
 	int championId;		
+	
 	// Champion Level
 	int championLevel;
+	
 	// Summoner Spell1
 	int summonerSpell1;
+	
 	// Summoner Spell2
 	int summonerSpell2;
+	
+	// Kill + Assist / Death
+	double Kda;
+	
+	// Main rune
+	int mainRune;
+	
+	// Auxiliary rune
+	int auxiliaryRune;
+	
+	// Items
+	List<Integer> items;
+	
+	// Total minions killed
+	int totalMinionsKilled;
+	
+	// Neutral minions killed
+	int neutralMinionsKilled;
+	
+	// Kill involvement rate
+	double killInvolvementRate;
+	
+	// Game duration(sec)
+	int gameDuration;
+	
+	// Game start timestamp
+	int gameStartTimestamp;
+	
+	// Queue id
+	int queueId;
+	
+	// Vision wards bought in game
+	int visionWardsBoughtInGame;
+	
+	// Summoners name in game
+	List<String> summonerName;
+	
+	// Champions id in game
+	List<Integer> championIds;
 	
 }
--- src/main/java/com/gsg/spring/dto/SummonerDto.java
+++ src/main/java/com/gsg/spring/dto/SummonerDto.java
@@ -9,26 +9,36 @@ public class SummonerDto {
 	
 	// Encrypted summoner ID
 	private String id;
+	
 	// Encrypted account ID
 	private String accountId;
+	
 	// Encrypted PUUID
 	private String puuid;
+	
 	// Summoner name
 	private String name;
 	// ID of the summoner icon 
 	private int profileIconId;
+	
 	// Summoner level 
 	private int summonerLevel;
+	
 	// Queue type
 	private String queueType;
+	
 	// Tier
 	private String tier;
+	
 	// League points
 	private int leaguePoints;
+	
 	// Winning team on Summoners Rift
 	private int wins;
+	
 	// Losing team on Summoners Rift
 	private int losses;
+	
 	// player's division within a tier
 	private String rank;
 			
